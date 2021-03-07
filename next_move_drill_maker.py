778da157a4fc896c4dedbbaf676f8368f07e78db
mnishz <mnishz@users.noreply.github.com>
2021-03-08 01:08:45 +0900

    最善手を kif 形式でも表示する

--- next_move_drill_maker.py
+++ next_move_drill_maker.py
@@ -6,6 +6,11 @@ import urllib.parse
 import Ayane.source.shogi.Ayane as ayane
 import python_shogi.shogi as shogi
 
+HANKAKU  = [ '1',  '2',  '3',  '4',  '5',  '6',  '7',  '8',  '9']
+ZENKAKU  = ['１', '２', '３', '４', '５', '６', '７', '８', '９']
+ALPHABET = [ 'a',  'b',  'c',  'd',  'e',  'f',  'g',  'h',  'i']
+KANJI    = ['一', '二', '三', '四', '五', '六', '七', '八', '九']
+
 def eval_sfen(sfen):
 
     time_to_think_ms = 2000
@@ -55,8 +60,8 @@ def eval_sfen(sfen):
         bestmove = usi.think_result.bestmove
 
         # 読み筋
-#         for i in range(num_yomisuji):
-#             print(usi.think_result.pvs[i].to_string())
+#         for candidate in range(num_yomisuji):
+#             print(usi.think_result.pvs[candidate].to_string())
 
         curr_eval = usi.think_result.pvs[0].eval * teban
 
@@ -80,10 +85,11 @@ def eval_sfen(sfen):
             url = "https://sfenreader.appspot.com/sfen?turn=off&sfen="
             url += urllib.parse.quote(board.sfen())
             last_move = str(board.peek())[2:4]
-            last_move = last_move[0] + str(ord(last_move[1]) - 96)
+            last_move = last_move[0] + str(ord(last_move[1]) - ord('a') + 1)
             url += "&lm=" + last_move
             print(url)
-            print("best move is " + last_bestmove)
+
+            print("best move is " + last_bestmove + ", " + get_kif_move(last_bestmove, board))
 #             break
 
         board.push_usi(move)
@@ -93,6 +99,28 @@ def eval_sfen(sfen):
         teban *= -1
 
     usi.disconnect()
+
+def get_kif_move(move, board):
+
+    # その到達地点に行ける同じ種類の駒が複数ある時の処理がない、右左上寄引
+
+    if move[1] is '*':
+        koma = shogi.PIECE_JAPANESE_SYMBOLS[shogi.PIECE_SYMBOLS.index(move[0].lower())]
+    else:
+        square = (9 - int(move[0])) + (ord(move[1]) - ord('a')) * 9
+        koma = board.piece_at(square).japanese_symbol()
+
+    kif = ZENKAKU[HANKAKU.index(move[2])]
+    kif += KANJI[ALPHABET.index(move[3])]
+    kif += koma
+
+    if len(move) == 5 and move[4] is '+':
+        kif += '成'
+
+    if move[1] is '*':
+        kif += '打'
+
+    return kif
 
 if __name__ == "__main__":
 
